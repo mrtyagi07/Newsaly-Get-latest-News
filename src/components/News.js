6970291445e56ec2728e1f5aafe96272a827ce1c
Vaibhav Tyagi <VAIBHAVTYAGI438@GMAIL.COM>
2022-12-09 19:29:56 +0530

    null

--- src/components/News.js
+++ src/components/News.js
@@ -83,7 +83,7 @@ const News = ({
         <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-5 ">
           {!loading &&
             data.map((element, i) => {
-              if (element.image_url == null) return;
+              if (element.image_url == null) return null;
               return (
                 <div className="w-full lg:max-w-full lg:flex" key={i}>
                   {
